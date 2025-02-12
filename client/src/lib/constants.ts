20a49ce86567c659c10c9c63bd2931c748e0121d
Node. Js <39589801-zoroontop30@users.noreply.replit.com>
2025-02-12 14:04:27 +0000

    Assistant checkpoint: Update artist quotes with new formatting
    Assistant generated file changes:
    - client/src/lib/constants.ts: Update artist quotes with new formatting
    
    ---
    
    User prompt:
    
    Here r the quotes make the texts between "" with the font i asked u for and everything
    The lines r gonna be
    Mac demarco : just to let u know "Ur my kind of women "
    Tv girl : i know we're "not allowed" to date but i just want u
    Mitski : i just "want you"
    Arctic monkeys: "i just wanna be urs"
    Alex g : ik we have " things to do" but i love u
    Tylor the creator: i will treat u better im not " like him"
    Adrianne lenker: i just want to be "anything" with u

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -21,7 +21,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "TV Girl",
     route: "tv-girl", 
-    lyric: "i know we're not allowed to date but i just want u",
+    lyric: "i know we're \"not allowed\" to date but i just want u",
     songName: "Not Allowed",
     image: "/artists/tv-girl.jpg", 
     audioUrl: "https://p.scdn.co/mp3-preview/6a46d469b67baa0fe945c0c41f1d45d368b8b57d",
@@ -30,7 +30,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "Mitski",
     route: "mitski",
-    lyric: "i just want you",
+    lyric: "i just \"want you\"",
     songName: "I Want You",
     image: "/artists/mitski.jpg",
     audioUrl: "https://p.scdn.co/mp3-preview/1c5b2e823e0acf5d7b6aa7df1b7e5364a0837026",
@@ -39,7 +39,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "Arctic Monkeys",
     route: "arctic-monkeys",
-    lyric: "i just wanna be urs",
+    lyric: "\"i just wanna be urs\"",
     songName: "I Wanna Be Yours",
     image: "/artists/arctic-monkeys.jpg",
     audioUrl: "https://p.scdn.co/mp3-preview/c12c4d92454bc8fdeea5d5a9a5b8da0995e6f26f",
@@ -48,7 +48,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "Alex G",
     route: "alex-g",
-    lyric: "ik we have things to do but i love u",
+    lyric: "ik we have \"things to do\" but i love u",
     songName: "Things to Do",
     image: "/artists/alex-g.jpg", 
     audioUrl: "https://p.scdn.co/mp3-preview/f5e7c96b50c5321e601bfec0a96c9e543ad8bee2",
@@ -57,7 +57,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "Tyler, The Creator",
     route: "tyler-the-creator",
-    lyric: "i will treat u better im not like him",
+    lyric: "i will treat u better im not \"like him\"",
     songName: "Like Him",
     image: "/artists/tyler.jpg",
     audioUrl: "https://p.scdn.co/mp3-preview/ba04068706dcd1f025df83f9bf71c5abe4f98ada",
@@ -66,7 +66,7 @@ export const ARTISTS: Artist[] = [
   {
     name: "Adrianne Lenker",
     route: "adrianne-lenker",
-    lyric: "i just want to be anything with u",
+    lyric: "i just want to be \"anything\" with u",
     songName: "Anything",
     image: "/artists/adrianne-lenker.jpg", 
     audioUrl: "https://p.scdn.co/mp3-preview/9c5cb4c974c87d9d88d87c0c6c3b20f76c87c5f1",
