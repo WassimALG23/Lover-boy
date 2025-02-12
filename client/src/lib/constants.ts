ed56941623f7de62f968b5143e60c9d8d5ddc8b1
hwhwhwu77 <39609405-hwhwhwu77@users.noreply.replit.com>
2025-02-12 15:30:20 +0000

    Agent query: Is the TV Girl image displaying correctly in the artist showcase?
    Update artist images and add audio URLs for improved functionality.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/f9d740a1-2d17-4fe6-a6c3-fceea80f6b2b/eae8853f-028b-4dd3-8323-e5e792e80c4c.jpg

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -4,7 +4,7 @@ export interface Artist {
   lyric: string;
   songName: string;
   image: string;
-  audioUrl?: string;  // Optional for now until user provides the audio files
+  audioUrl?: string;  // Optional for now until we add working audio files
   quoteColor: string;
 }
 
@@ -15,6 +15,7 @@ export const ARTISTS: Artist[] = [
     lyric: "just to let u know Ur my kind of women",
     songName: "My Kind of Woman",
     image: "/artists/mac-demarco.jpg",
+    audioUrl: "/audio/mac-demarco-my-kind-of-woman.mp3",
     quoteColor: "text-emerald-400"
   },
   {
@@ -22,7 +23,8 @@ export const ARTISTS: Artist[] = [
     route: "tv-girl", 
     lyric: "i know that we're not allowed to but i want you",
     songName: "Not Allowed",
-    image: "/artists/tv-girl.jpg",
+    image: "/attached_assets/Screenshot_2025-02-12-12-43-54-811_com.spotify.music.png",
+    audioUrl: "/audio/tv-girl-not-allowed.mp3",
     quoteColor: "text-rose-400"
   },
   {
@@ -31,6 +33,7 @@ export const ARTISTS: Artist[] = [
     lyric: "i just want you",
     songName: "I Want You",
     image: "/artists/mitski.jpg",
+    audioUrl: "/audio/mitski-i-want-you.mp3",
     quoteColor: "text-blue-400"
   },
   {
@@ -39,6 +42,7 @@ export const ARTISTS: Artist[] = [
     lyric: "i will treat u better im not like him",
     songName: "Like Him",
     image: "/artists/tyler.jpg",
+    audioUrl: "/audio/tyler-like-him.mp3",
     quoteColor: "text-cyan-400"
   },
   {
@@ -47,6 +51,7 @@ export const ARTISTS: Artist[] = [
     lyric: "i just want to be anything with u",
     songName: "Anything",
     image: "/artists/adrianne-lenker.jpg",
+    audioUrl: "/audio/adrianne-lenker-anything.mp3",
     quoteColor: "text-orange-400"
   },
   {
@@ -55,6 +60,7 @@ export const ARTISTS: Artist[] = [
     lyric: "i wanna be yours",
     songName: "I Wanna Be Yours",
     image: "/artists/arctic-monkeys.jpg",
+    audioUrl: "/audio/arctic-monkeys-i-wanna-be-yours.mp3",
     quoteColor: "text-purple-400"
   }
 ];
